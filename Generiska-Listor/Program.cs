1165bc4a61d4b2e90d9b2742a9cb8ce78d67962b
L S <61838582+leon-bot-max@users.noreply.github.com>
2021-03-25 13:43:19 +0100

    Add project files.

---
+++ Generiska-Listor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generiska_Listor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
